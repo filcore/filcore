cfe2946c301cf4afe6336add49dc2378dea8cbf5
cgpvibe <cgpvibe@gmail.com>
2025-10-27 05:05:45 +0900

    레이아웃

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link';
 
 export const metadata = {
   title: "FilCore | MF & UF membrane with Filcore",
@@ -12,7 +13,41 @@ export default function RootLayout({ children }) {
       <body
         className={`antialiased`}
       >
-        {children}
+        <div className='fixed top-0 left-0 w-full'>
+          <div className='container mx-auto flex items-center justify-between h-26'>
+            <div>
+              <Link href="/">
+                <img src="/logo.png" alt="" className='w-[180px]' />
+              </Link>
+            </div>
+            <ul className='flex items-center gap-10 font-semibold'>
+              <li>
+                <Link href="/">인사말</Link>
+              </li>
+              <li>
+                <Link href="/">제품소개</Link>
+              </li>
+              <li>
+                <Link href="/">기술연구소</Link>
+              </li>
+              <li>
+                <Link href="/">필코아소식</Link>
+              </li>
+              <li>
+                <Link href="/">고객센터</Link>
+              </li>
+              <li>
+                <Link href="/">ENG</Link>
+              </li>
+              <li>
+                <Link href="/" className='bg-[#54a8c7] text-white p-3 rounded-md'>Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='w-full'>
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      sss
-    </div>
+    <section className='sectionHero w-full min-h-[500px] bg-[#eff7fa] pt-32'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-2'>
+          <div></div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
