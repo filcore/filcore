d2679776570f5cbff5f6887c7de0f6b77fb71c5c
cgpvibe <cgpvibe@gmail.com>
2025-10-31 01:01:08 +0900

    파비콘 작업

---
+++ src/app/head.js
@@ -0,0 +1,16 @@
+// src/app/head.js
+export default function Head() {
+  return (
+    <>
+      <title>Filcore</title>
+      <meta name="description" content="Filcore" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      {/* 파비콘 */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    </>
+  );
+}
